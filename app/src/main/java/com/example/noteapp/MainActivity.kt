26cea90289fa66605ad7ddc50c3ea041ecc2dff3
Ayaaz <ayaazansari3052@gmail.com>
2020-12-16 23:44:12 +0530

    database commit 2

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         viewModel = ViewModelProvider(this,
+         ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel)
 
     }
 }
--- app/src/main/java/com/example/noteapp/NoteRepository.kt
+++ app/src/main/java/com/example/noteapp/NoteRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.LiveData
 //handles data from multiple resources.
 class NoteRepository(private val noteDao:NoteDao) {
 
+
     val allNotes:LiveData<List<Note>> = noteDao.getAllNotes()
 
     suspend fun insert(note:Note)
